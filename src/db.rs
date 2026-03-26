041560c45787ab08d328d01554a994cb6f558ca2
Kamil Pomykała <kamil@kamilpomykala.dev>
2026-03-27 00:29:05 +0100

    feat: Add database migration initialization
    - Introduced `init` function in `db.rs` to run pending migrations.
    - Updated `main.rs` to initialize database migrations during startup.

--- src/db.rs
+++ src/db.rs
@@ -1,5 +1,13 @@
 use diesel::prelude::*;
+use diesel_migrations::{EmbeddedMigrations, MigrationHarness, embed_migrations};
 use std::env;
+
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("migrations");
+pub fn init() {
+    let mut conn = establish_connection();
+    conn.run_pending_migrations(MIGRATIONS)
+        .expect("Failed to run database migrations");
+}
 
 pub fn establish_connection() -> SqliteConnection {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use laneya::{scheduler, tracer, web_server};
+
+use laneya::{db, scheduler, tracer, web_server};
 use laneya::app_state::AppState;
 
 #[tokio::main]
 async fn main() {
     dotenvy::dotenv().ok();
     tracer::init();
+    db::init();
 
     let state = AppState::new();
     scheduler::start_feed_refresh_job(state.feed_tx.clone());
